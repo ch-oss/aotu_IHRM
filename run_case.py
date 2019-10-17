3f1d8a730e74b88200739a534ded6e625805e174
Administrator <maidangnani@163.com>
2019-10-17 17:29:13 +0800

    v1.0

--- run_case.py
+++ run_case.py
@@ -8,7 +8,7 @@ from case.test_case import TestIhrm
 suite = unittest.TestSuite()
 
 suite.addTest(unittest.makeSuite(TestIhrm))
-report_name = time.strftime("%Y%m%d%H%M%S")
+# report_name = time.strftime("%Y%m%d%H%M%S")
 
 BeautifulReport(suite).report\
-    (description="chorm",filename=str(report_name),log_path="./")
+    (description="chorm",filename="my_report.html",log_path="./report")
